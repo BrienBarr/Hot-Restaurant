3bba1f327125588ee72b3c697c363d2215bfb915
John Ciancone <jackccinc@gmail.com>
2021-01-25 20:12:59 -0500

    now without /api

--- backend.js
+++ backend.js
@@ -15,11 +15,11 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
   });
 
-  app.get("/api/tables", function(req, res) {
+  app.get("/tables", function(req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
   });
 
-  app.get("api/reserve", function(req, res) {
+  app.get("/reserve", function(req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
   });
 
